63e44c508a26122adfbe13275d439110bcbedc49
PaaulCB <>
2023-11-23 21:17:22 +0000

    Add Board class

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 class Ship():
     """
     Creates an instance of Ship
@@ -31,3 +34,22 @@ class SmallShip(Ship):
     def __init__(self, coordinates):
         super().__init__(coordinates)
         self.value = 3
+
+
+class Board():
+    """
+    Creates an instance of Board
+    """
+    def __init__(self, name, type, size):
+        self.name = name
+        self.type = type
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.ships = []
+        self.guesses = []
+
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
