a6e5b88431ea4e03cdf3133359eafbd3fd2303eb
PaaulCB <>
2023-11-23 20:20:28 +0000

    Add Ship class and SmallShip, MediumShip and BigShip subclasses

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Ship():
+    """
+    Creates an instance of Ship
+    """
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
+
+class BigShip(Ship):
+    """
+    Subclass of the superclass Ship
+    """
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
+        self.value = 1
+
+
+class MediumShip(Ship):
+    """
+    Subclass of the superclass Ship
+    """
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
+        self.value = 2
+
+
+class SmallShip(Ship):
+    """
+    Subclass of the superclass Ship
+    """
+    def __init__(self, coordinates):
+        super().__init__(coordinates)
+        self.value = 3
